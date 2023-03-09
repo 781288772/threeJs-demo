133600c521c738e6ebd5d4501d4f4a423d1933d9
陈云 <t21cy2010@163.com>
2023-03-09 15:29:44 +0800

    feat:绘制模型

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref,onMounted } from 'vue'
 import * as THREE from "three"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 defineProps<{ msg: string }>()
 
 onMounted(()=>{
@@ -14,16 +16,59 @@ const  initThree = ()=> {
       const canvas = document.querySelector('#three')
       const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
       const camera = new THREE.PerspectiveCamera(
-        50,
+        25,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
       )
-      camera.position.z = 10
+      camera.position.z = 10;
 
+      const gltfLoader = new GLTFLoader()
+      gltfLoader.load('/public/1.gltf', (gltf) => {
+        var model = gltf.scene
+           //遍历模型每部分
+           model.traverse((o) => {
+          //将图片作为纹理加载
+          let explosionTexture = new THREE.TextureLoader().load(
+            '/public/textures/potted_plant_01_leaves_diff_4k.jpg'
+          )
+          //调整纹理图的方向
+          explosionTexture.flipY = false
+          //将纹理图生成基础网格材质(MeshBasicMaterial)
+          const material = new THREE.MeshBasicMaterial({
+            map: explosionTexture,
+          })
+          //给模型每部分上材质
+          o.material = material
+        })
+        scene.add(model)
+      })
+      function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement
+        var width = window.innerWidth
+        var height = window.innerHeight
+        var canvasPixelWidth = canvas.width / window.devicePixelRatio
+        var canvasPixelHeight = canvas.height / window.devicePixelRatio
+
+        const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height
+        if (needResize) {
+          renderer.setSize(width, height, false)
+        }
+        return needResize
+      }
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.enableDamping = true
       function animate() {
+        controls.update()
         renderer.render(scene, camera)
         requestAnimationFrame(animate)
+
+        //添加下面代码
+        if (resizeRendererToDisplaySize(renderer)) {
+          const canvas = renderer.domElement
+          camera.aspect = canvas.clientWidth / canvas.clientHeight
+          camera.updateProjectionMatrix()
+        }
       }
       animate()
     }
