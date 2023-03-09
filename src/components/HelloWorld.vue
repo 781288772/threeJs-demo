1918d0354549e290aa9e1a51dd740e13d7d132e0
陈云 <t21cy2010@163.com>
2023-03-09 14:42:25 +0800

    feat:绘制一片区域

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
+import { ref,onMounted } from 'vue'
+import * as THREE from "three"
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+onMounted(()=>{
+  initThree();
+})
+
+
+const  initThree = ()=> {
+      const scene = new THREE.Scene()
+      scene.background = new THREE.Color('#eee')
+      const canvas = document.querySelector('#three')
+      const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+      const camera = new THREE.PerspectiveCamera(
+        50,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      )
+      camera.position.z = 10
+
+      function animate() {
+        renderer.render(scene, camera)
+        requestAnimationFrame(animate)
+      }
+      animate()
+    }
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <canvas id="three"></canvas>
 </template>
 
 <style scoped>
